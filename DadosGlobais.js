0e95f8e667a46d22641bf39d3e421a64da5c2812
tazianeroberta <137331870+tazianeroberta@users.noreply.github.com>
2024-10-22 16:48:46 -0300

    22/10

--- DadosGlobais.js
+++ DadosGlobais.js
@@ -8,9 +8,9 @@ async function vizualizarInformacoesGlobais (){
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container__texto');
     paragrafo.innerHTML = `Vocẽ sabia que o mundo tem 
-    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente
-    ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média 
-    ${dados.tempo_medio} horas conectadas`
+    <span>${dados.total_pessoas_mundo}</span> de pessoas e que aproximadamente
+    <span> ${dados.total_pessoas_conectadas}</span> estão conectadas em alguma rede social e passam em média 
+    <span>${dados.tempo_medio}</span> horas conectadas`
 
     const container = document.getElementById('graficos-container');
     container.appendChild(paragrafo);
